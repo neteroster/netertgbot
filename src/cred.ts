14c7da418a9aa1fc35c0aef865b0b86616845ce9
NeterOster <neteroster@gmail.com>
2023-07-20 08:12:15 +0800

    Create cred.ts

---
+++ src/cred.ts
@@ -0,0 +1,13 @@
+import { Configuration } from "openai";
+
+
+/* Telegram */
+export const TELEGRAM_BOT_TOKEN = "";
+
+
+/* OpenAI */
+export const OPENAI_CONFIG = new Configuration({
+    apiKey: "",
+    basePath: "https://api.openai-sb.com/v1"
+});
+
